f92fde7aae5f7aad35d7271d6fc08a97e8005c6f
dianahasiuk <diana.hasiuk.23@pnu.edu.ua>
2026-05-16 20:10:28 +0300

    Update lab5p2.py

--- lab5/lab5p2.py
+++ lab5/lab5p2.py
@@ -11,17 +11,16 @@ def main():
     rg = "az104-rg5"
 
     commands = [
-        # Task 4 - VNet Peering (двосторонній)
-        # Лінк з боку CoreServicesVnet: ManufacturingVnet-to-CoreServicesVnet
+      
         f"az network vnet peering create -g {rg} -n ManufacturingVnet-to-CoreServicesVnet --vnet-name CoreServicesVnet --remote-vnet ManufacturingVnet --allow-vnet-access --allow-forwarded-traffic",
-        # Лінк з боку ManufacturingVnet: CoreServicesVnet-to-ManufacturingVnet
+      
         f"az network vnet peering create -g {rg} -n CoreServicesVnet-to-ManufacturingVnet --vnet-name ManufacturingVnet --remote-vnet CoreServicesVnet --allow-vnet-access --allow-forwarded-traffic",
 
-        # Task 6 - UDR: таблиця маршрутів rt-CoreServices
+       
         f"az network route-table create -g {rg} -n rt-CoreServices --disable-bgp-route-propagation true",
-        # Маршрут PerimetertoCore: через NVA 10.0.1.7
+       
         f"az network route-table route create -g {rg} --route-table-name rt-CoreServices -n PerimetertoCore --address-prefix 10.0.0.0/16 --next-hop-type VirtualAppliance --next-hop-ip-address 10.0.1.7",
-        # Прив'язка таблиці до підмережі perimeter
+        
         f"az network vnet subnet update -g {rg} --vnet-name CoreServicesVnet -n perimeter --route-table rt-CoreServices",
     ]
 
